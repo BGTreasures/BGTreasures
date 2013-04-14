5c2bd3ed23530968bedd80f491eeb7e6813397dc
Arron Green <arrong@gmail.com>
2013-04-14 19:11:06 -0400

    work to templates for gallery

--- bgtreasures/bgtreasures/views.py
+++ bgtreasures/bgtreasures/views.py
@@ -10,3 +10,15 @@ def home(request):
 def about(request):
     return render_to_response('about.html', {}, context_instance=RequestContext(request))
 
+def gallery(request, gallery=None):
+    v = ""
+    if gallery == "bridal":
+        v = "%s_bridal.html" % "gallery"
+    elif gallery == "memorial":
+        v = "%s_memorial.html" % "gallery"
+    elif gallery == "original_art":
+        v = "%s_original_art.html" % "gallery"
+    elif gallery == "other":
+        v = "%s_other.html" % "gallery"
+    return render_to_response(v, {}, context_instance=RequestContext(request))
+
