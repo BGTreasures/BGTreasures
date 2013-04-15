d3eae5a48f2510ab6ffebcc2d3c8757641a763a6
Arron Green <arrong@gmail.com>
2013-04-14 20:36:42 -0400

    added django sqlite sample

--- bgtreasures/bgtreasures/settings.py
+++ bgtreasures/bgtreasures/settings.py
@@ -97,6 +97,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
     'django.contrib.admin',
+    'person',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
