95bf63cc06afe3ff3bac9536788cfc4981872cf5
Arron Green <arrong@gmail.com>
2013-05-19 13:57:09 -0400

    more work to carousel

--- bgtreasures/bgtreasures/models.py
+++ bgtreasures/bgtreasures/models.py
@@ -1,6 +1,12 @@
 from django import forms
+from django.db import models
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
     email = forms.EmailField()
     message = forms.CharField(max_length=500)
+
+class HomeCarousel(models.Model):
+    name = models.CharField(max_length=50)
+    alt = models.CharField(max_length=50)
+    descrip = models.CharField(max_length=50)
