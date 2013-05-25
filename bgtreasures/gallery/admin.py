26de51cdda9a0dfaa3dc5e13bec8f1956af6f77d
Arron Green <arrong@gmail.com>
2013-05-25 16:10:46 -0400

    added gallery item images

--- bgtreasures/gallery/admin.py
+++ bgtreasures/gallery/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from models import GalleryCategory, GalleryItem, GalleryItemModifier
+from models import *
 
 admin.site.register(GalleryCategory)
 admin.site.register(GalleryItem)
+admin.site.register(GalleryItemImage)
 admin.site.register(GalleryItemModifier)
