b19505a448704c0a2593328fd69f9481626571dd
Arron Green <arrong@gmail.com>
2013-05-29 20:26:13 -0400

    moved contact into it's own app

---
+++ bgtreasures/contact/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls.defaults import *
+from views import *
+
+urlpatterns = patterns('contact.views',
+    url(r'^$', contact, name='contact'),
+    url(r'^/success$', contact_success, name='contact_success'),
+)
