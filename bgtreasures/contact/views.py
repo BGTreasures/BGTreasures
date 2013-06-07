f7026879a5e41322e4c826afd7cfdc3c040026b8
Arron Green <arrong@gmail.com>
2013-06-06 22:21:29 -0400

    last experiment before i give up

--- bgtreasures/contact/views.py
+++ bgtreasures/contact/views.py
@@ -23,9 +23,7 @@ def contact(request):
             try:
                 email_message = settings.EMAIL_TEMPLATE.format(**data)
                 bcc = [settings.ADMIN_EMAIL_TO]
-                email_split = recipient_email.split('@')
-                from_email = "".join((email_split[0], "+", "@", email_split[1]))
-                msg = EmailMessage(settings.EMAIL_SUBJECT, email_message, from_email, [from_email], bcc, 
+                msg = EmailMessage(settings.EMAIL_SUBJECT, email_message, data['email'], send_to, bcc, 
                         headers = {'Reply-To': data['email']})
                 msg.send()
                 return HttpResponseRedirect(reverse('contact_success'))
