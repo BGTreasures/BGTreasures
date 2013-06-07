7ca777ae1cbf0cee1b64806365a4ff09fa9a4a23
Arron Green <arrong@gmail.com>
2013-06-07 09:33:25 -0400

    revisions to links app models

--- bgtreasures/links/models.py
+++ bgtreasures/links/models.py
@@ -2,16 +2,16 @@ from django.db import models
 import datetime
 
 class LinkCategory(models.Model):
-    title = models.CharField(max_length=50, allow_null=True)
-    order = models.IntField(default=10)
+    title = models.CharField(max_length=50, unique=True, null=True)
+    order = models.IntegerField(default=10)
     added_on = models.DateTimeField(default=datetime.datetime.now)
     updated_on = models.DateTimeField(default=datetime.datetime.now)
 
 class LinkItem(models.Model):
     category = models.ForeignKey(LinkCategory)
     resource_title = models.CharField(max_length=50)
-    resource_link = models.LinkField()
-    resource_description = models.CharField(max_length=200, allow_empty=True)
-    order = models.IntField(default=10)
+    resource_url = models.URLField()
+    resource_description = models.CharField(max_length=200, blank=True)
+    order = models.IntegerField(default=10)
     added_on = models.DateTimeField(default=datetime.datetime.now)
     updated_on = models.DateTimeField(default=datetime.datetime.now)
