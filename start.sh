369e88551845ca3a3bbe3f6ad50e094e4ca81472
Betty Green <bettyl.green@Bettys-MacBook-Pro.local>
2018-11-04 11:19:51 -0500

    modifications to start

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-source bin/activate
-pip install -r requirements.txt
+# source bin/activate
+# pip install -r requirements.txt
 python bgtreasures/manage.py syncdb
 python bgtreasures/manage.py migrate links
 python bgtreasures/manage.py runserver
